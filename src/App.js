dab2611753301b0e5db8d8d53e353bbc92da78bb
Bob Zurad <bob@zurad.net>
2017-08-31 15:40:01 -0500

    bootstrap cover example

--- src/App.js
+++ src/App.js
@@ -5,31 +5,37 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">React App</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to React</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://react-bootstrap.github.io/components.html"
-                target="_blank">
-                View React Bootstrap Docs
-              </Button>
+      <div className="site-wrapper-inner">
+        <div className="cover-container">
+
+          <div className="masthead clearfix">
+            <div className="inner">
+              <h3 className="masthead-brand">Cover</h3>
+              <nav>
+                <ul className="nav masthead-nav">
+                  <li className="active"><a href="#">Home</a></li>
+                  <li><a href="#">Features</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div className="inner cover">
+            <h1 className="cover-heading">Cover your page.</h1>
+            <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+            <p className="lead">
+              <a href="#" className="btn btn-lg btn-default">Learn more</a>
             </p>
-          </Grid>
-        </Jumbotron>
+          </div>
+
+          <div className="mastfoot">
+            <div className="inner">
+              <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+            </div>
+          </div>
+
+        </div>
       </div>
     );
   }
