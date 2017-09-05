0fae6d0e11ca63dc6e67f1ae9b5ab58d0feaa1a2
Bob Zurad <bob@zurad.net>
2017-09-05 16:12:15 -0500

    added app info and changed link color

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
                 <ul className="nav masthead-nav">
                   <li><a href="https://github.com/bobzurad">GitHub</a></li>
                   <li><a href="https://twitter.com/bobzurad">Twitter</a></li>
-                  <li><a href="https://instagram.com/bobzurad">Instagram</a></li>
+                  <li className="hiddenBelow400"><a href="https://instagram.com/bobzurad">Instagram</a></li>
                   <li><a href={resume}>Resume</a></li>
                 </ul>
               </nav>
@@ -26,11 +26,39 @@ class App extends Component {
           <div className="inner cover">
             <h1 className="cover-heading">Hi. I'm Bob.</h1>
             <p className="lead">
-              When I'm not spending time with my wife and daughter, I like to <a href="https://github.com/bobzurad">write code</a> and <a href="http://steamcommunity.com/id/spurioustuple/">play video games.</a> I'm currently accepting work with <a href="https://stackoverflow.blog/2017/02/08/means-remote-first-company/">remote-first</a> employers. If you like what you see on my resume or GitHub, please drop me a line.
+              When I'm not spending time with my wife and daughter, I like to <a href="https://github.com/bobzurad">write code</a> and <a href="http://steamcommunity.com/id/spurioustuple/">play video games.</a> I'm currently accepting opportunities with <a href="https://stackoverflow.blog/2017/02/08/means-remote-first-company/">remote-first</a> employers. If you like what you see on my resume or GitHub, please drop me a line. Peace.
             </p>
-            <p className="lead">
-              Peace
-            </p>
+
+            <h2 className="appsHeader">Apps I've Created</h2>
+            <div className="row">
+              <div className="col-md-4 appInfo">
+                <h3><a href="https://www.notefireapp.com">NoteFire</a></h3>
+                <p>
+                  A lightweight notes app that stores data in the cloud. 
+                </p>
+                <p>
+                  <a href="https://github.com/bobzurad/NoteFire">Source Code</a>
+                </p>
+              </div>
+              <div className="col-md-4 appInfo">
+                <h3><a href="https://play.google.com/store/apps/details?id=net.zurad.bob.whitenoisenightlight">Night Light</a></h3>
+                <p>
+                  An Android app that allows the device to be used as a night light and provide white noise.
+                </p>
+                <p>
+                  <a href="https://github.com/bobzurad/WhiteNoiseNightLight">Source Code</a>
+                </p>
+              </div>
+              <div className="col-md-4 appInfo">
+                <h3><a href="https://www.microsoft.com/en-us/store/p/abv-calculator/9wzdncrdmb7q">ABV Calculator</a></h3>
+                <p>
+                  A Windows app that calculates the ABV of homebrew, given the Original Gravity and Final Gravity.
+                </p>
+                <p>
+                  <a href="https://github.com/bobzurad/NoteFire">Source Code</a>
+                </p>
+              </div>
+            </div>
           </div>
 
           <div className="mastfoot">
